ef01ccda62c88afec4a853730e753881d303ac99
Denys Plietnikov <givisimo@gmail.com>
2019-11-23 01:19:27 +0200

    Updates

---
+++ precache-manifest.baa2679fc77639778e7ce08502ec3344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cbe8cc4cd0fdcdf8f761e83775b5d0",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "dd662cd45373ed9a5b38",
+    "url": "/goit-react-hw-01-components/static/css/main.8c361027.chunk.css"
+  },
+  {
+    "revision": "2d4016473b7450d0273b",
+    "url": "/goit-react-hw-01-components/static/js/2.ddce9293.chunk.js"
+  },
+  {
+    "revision": "dd662cd45373ed9a5b38",
+    "url": "/goit-react-hw-01-components/static/js/main.1a7ad1c4.chunk.js"
+  },
+  {
+    "revision": "75ab52edfcbe0282d12c",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.b133d7bc.js"
+  }
+]);
